fb197c2bd2a49a70759f75223ad1793b32ce8aa1
tolgakurtarmis <TolgaKurtarmis@DESKTOP-UUC69MN>
2021-10-25 02:05:29 +0300

    codework

--- CodeWork/App_Start/RouteConfig.cs
+++ CodeWork/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CodeWork
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Book", action = "Add", id = UrlParameter.Optional }
             ); 
         }
     }
